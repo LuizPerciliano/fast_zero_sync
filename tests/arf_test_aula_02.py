b9965cf82fd2b218319cfabda53b8a01817904dd
Perciliano <luizperciliano@gmail.com>
2024-07-15 23:48:58 -0300

    Aula 03 - Implementando rotas do CRUD

--- tests/arf_test_aula_02.py
+++ tests/arf_test_aula_02.py
@@ -1,14 +1,13 @@
 from http import HTTPStatus
 
 from fastapi.responses import HTMLResponse
-
 from fastapi.testclient import TestClient
 
-from fast_zero.aula_02 import aula
+from fast_zero.aula_02 import aula_02
 
 
 def test_root_deve_retornar_ok_e_ola_mundo_em_html():
-    client = TestClient(aula)
+    client = TestClient(aula_02)
 
     response = client.get('/', response_class=HTMLResponse)
 
